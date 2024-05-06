9457ea5bc3a5f6845e8c2d7fda6e170e57512786
Igor <64653230+IgorTan14@users.noreply.github.com>
2024-05-06 15:23:26 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,111 @@
+$(function(){
+   $('.benefits__more-open').on('click', function(){
+    $('.benefits__inner-bottom').addClass('active-grid');
+    $('.benefits__more-open').addClass('none');
+    $('.benefits__more-close').addClass('active-block');
+   })
+
+   $('.benefits__more-close').on('click', function(){
+    $('.benefits__inner-bottom').removeClass('active-grid');
+    $('.benefits__more-close').removeClass('active-block');
+    $('.benefits__more-open').removeClass('none');
+   })
+
+   $('.gallery__inner').slick({
+     arrows: true,
+     dots: false,
+     nextArrow: '<button type="button" class="slick-next slick-arr"><img src="images/icons/arr-left.svg" alt="next"></button>',
+     prevArrow: '<button type="button" class="slick-prev slick-arr"><img src="images/icons/arr-right.svg" alt="prev"></button>',
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     Infinite: true,
+     responsive: [
+        {
+          breakpoint: 600,
+          settings: {
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 2000,
+                 }
+        }
+     ],
+});
+
+$('.consultation__inner').slick({
+  arrows: false,
+  dots: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  Infinite: true,
+});
+
+$('.team__slider').slick({
+  arrows: false,
+  dots: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  Infinite: true,
+  responsive: [
+    {
+      breakpoint: 870,
+      settings: {
+        slidesToShow: 2,
+             }
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 1,
+             }
+    }
+ ],
+});
+
+$('.team__slider-info-slider-inner').slick({
+  arrows: true,
+  dots: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  Infinite: true,
+  nextArrow: '<button type="button" class="team__slider-next slick-arr"><img src="images/icons/arr-left.svg" alt="next"></button>',
+  prevArrow: '<button type="button" class="team__slider-prev slick-arr"><img src="images/icons/arr-right.svg" alt="prev"></button>',
+});
+
+$('.revies__inner').slick({
+  arrows: true,
+  dots: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  Infinite: true,
+  nextArrow: '<button type="button" class="team__slider-next slick-arr"><img src="images/icons/arr-left.svg" alt="next"></button>',
+  prevArrow: '<button type="button" class="team__slider-prev slick-arr"><img src="images/icons/arr-right.svg" alt="prev"></button>',
+});
+
+
+$('.header__burger').on('click', function(){ 
+  $('.burger__content').toggleClass('burger__content--active');
+
+  // $('.product-list__filter ').toggleClass('hidden');
+  // $('.main').toggleClass('off');
+  $('.header__burger').toggleClass('header__burger--active');
+});
+
+});
+
+let map;
+
+async function initMap() {
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+initMap();
